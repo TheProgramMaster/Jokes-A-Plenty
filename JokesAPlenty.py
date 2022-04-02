78c5975f32d0c5ba9aa5e9c734f3838bd3892a06
TheProgramMaster <72845454+TheProgramMaster@users.noreply.github.com>
2022-04-01 21:21:20 -0500

    Finished Python Bot

--- JokesAPlenty.py
+++ JokesAPlenty.py
@@ -4,7 +4,7 @@ from jokeapi import Jokes # Import the Jokes class
 import asyncio
 async def print_joke():
     j = await Jokes()  # Initialise the class
-    joke = await j.get_joke(blacklist=['nsfw','racist','sexist','religious','political'])  # Retrieve a random joke
+    joke = await j.get_joke(blacklist=['nsfw','racist','sexist','religious','political','explicit'])  # Retrieve a random joke
     if joke["type"] == "single": # Print the joke
         print(joke["joke"])
         tts = gtts.gTTS(joke["joke"],lang="en")
@@ -13,5 +13,8 @@ async def print_joke():
     else:
         print(joke["setup"])
         print(joke["delivery"])
+        tts = gtts.gTTS(joke["setup"]+" " + joke["delivery"],lang="en")
+        tts.save("joke.mp3")
+        playsound("joke.mp3")
 
 asyncio.run(print_joke())
