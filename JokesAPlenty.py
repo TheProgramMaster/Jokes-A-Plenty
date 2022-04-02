bec92e2405d80879712dab99c4ac290ec9b0bf5b
TheProgramMaster <72845454+TheProgramMaster@users.noreply.github.com>
2022-04-01 20:50:36 -0500

    Fixed Python Bot Errors

--- JokesAPlenty.py
+++ JokesAPlenty.py
@@ -1,11 +1,11 @@
+import gtts
 from playsound import playsound
 from jokeapi import Jokes # Import the Jokes class
 import asyncio
-import gtts
 async def print_joke():
     j = await Jokes()  # Initialise the class
     joke = await j.get_joke(blacklist=['nsfw','racist','sexist','religious','political'])  # Retrieve a random joke
-    if joke["type"] == "single" or "twopart" or "Any": # Print the joke
+    if joke["type"] == "single": # Print the joke
         print(joke["joke"])
         tts = gtts.gTTS(joke["joke"],lang="en")
         tts.save("joke.mp3")
